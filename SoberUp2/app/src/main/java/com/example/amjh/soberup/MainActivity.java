c228d29fffa9de214bee21b244612f8421ea545b
Jenell-M-Hogg <jenellmhogg@live.com>
2017-04-17 01:54:25 -0400

    Adding Disclaimer Screen
    Rough draft

--- SoberUp2/app/src/main/java/com/example/amjh/soberup/MainActivity.java
+++ SoberUp2/app/src/main/java/com/example/amjh/soberup/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.amjh.soberup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+
+        android.widget.TextView myT = (TextView) findViewById(R.id.myTextView1);
+        myT.setText("Hullo this is your sobering up friend");
+
+
+
+
     }
 }
